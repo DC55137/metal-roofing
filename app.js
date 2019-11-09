90c87f41b8ed78bf4d1532e579d08f521ff1b4bb
Daniel Correa <danielcorrea@Daniels-MacBook-Pro.local>
2019-11-10 01:19:36 +1000

    views added

---
+++ app.js
@@ -0,0 +1,40 @@
+var express = require("express");
+var app = express();
+
+app.use(express.static("public"));
+
+app.set('view engine', 'ejs');
+
+app.get("/", function(req,res){
+	res.render("Home");
+})
+
+app.get("/About", function(req,res){
+	res.render("About");
+})
+
+app.get("/Gallery", function(req,res){
+	res.render("Gallery");
+})
+
+app.get("/Gallery/Southport", function(req,res){
+	res.render("Southport");
+})
+
+app.get("/Gallery/Coombabah", function(req,res){
+	res.render("Coombabah");
+})
+
+app.get("/Gallery/Alstonville", function(req,res){
+	res.render("Alstonville");
+})
+
+
+app.get("*", function(req,res){
+	res.render("unknown");
+})
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Server Has Started!");
+});
