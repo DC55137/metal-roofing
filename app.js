876ec0c5e5c4da0a4da3a94e7524657a7dd47730
Daniel Correa <daniel.correa55137@gmail.com>
2020-07-21 16:54:08 +1000

    Email Adjusted

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
-const sendgridTransport = require('nodemailer-sendgrid-transport');
+require("dotenv").config();
 
+console.log(process.env.EMAIL)
+console.log(process.env.PASSWORD)
 
-const transporter = nodemailer.createTransport(sendgridTransport({
+const transporter = nodemailer.createTransport({
+	service: 'gmail',
 	auth: {
-		api_key: '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'
+		user: process.env.EMAIL,
+		pass: process.env.PASSWORD
 	}
-}))
+})
 const app = express();
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
@@ -21,76 +25,118 @@ app.post("/createQuote", urlencodedParser, (req, res)=>{
 	console.log(req.body);
 	const body = req.body;
 	console.log(body)
-	const body2 = "<h1>"+ body["First Name"]+" "+ body["Last Name"]+"</h1><br><h2> Email: "+ body["Email"]+" Number: "+ body["Number-quote"]+"</h2><br><p>"+ " "+body["Message"] +"</p><br><br><h4> Email send through Website </h4>";
+	const body2 = "<h1>"+ body["First Name"]+" "+ body["Last Name"]+"</h1><br><h2> Email: "+ body["Email"]+" Number: "+ body["Number-quote"]+"</h2><br><p>"+ " "+body["Message"] +"</p><br><br><h4> Email sent through Website </h4>";
 	transporter.sendMail({
 		to: "daniel.correa55137@gmail.com",
-		from: "cbroofing@hotmail.com",
+		from: "daniel.cbroofing@gmail.com",
 		subject: "quote recieved",
 		html: body2
+	}, (err, data)=>{
+		if(err){
+			console.log('error occurs sending', err)
+		}else{
+			console.log("email sent")
+		}
 	})
 	console.log(body2);
-	res.render("Home");
+	res.render("Home", {
+		title: "Home"
+	});
 })
 
 app.get("/", function(req,res){
-	res.render("Home");
+	res.render("Home", {
+		title:'Home'
+	});
 })
 
 app.get("/Quote", function(req,res){
-	res.render("Quote");
+	res.render("Quote", {
+		title: 'Quote'
+	});
 })
 
 app.get("/About", function(req,res){
-	res.render("about");
+	res.render("about", {
+		title: "about"
+	});
 })
 
 app.get("/Gutter", function(req,res){
-	res.render("Gutter");
+	res.render("Gutter", {
+		title: "services"
+	});
 })
 
 app.get("/DownPipes", function(req,res){
-	res.render("DownPipes");
+	res.render("DownPipes", {
+		title: "services"
+	});
 })
 app.get("/SkyLights", function(req,res){
-	res.render("Sky Light");
+	res.render("Sky Light", {
+		title: "services"
+	});
 })
 
 app.get("/WhirlyBirds", function(req,res){
-	res.render("Whirly Birds");
+	res.render("Whirly Birds", {
+		title: "services"
+	});
 })
 app.get("/Insulation", function(req,res){
-	res.render("Insulation");
+	res.render("Insulation", {
+		title: "services"
+	});
 })
 app.get("/RoofAnchors", function(req,res){
-	res.render("Roof Anchors");
+	res.render("Roof Anchors", {
+		title: "services"
+	});
 })
 app.get("/RoofWalkways", function(req,res){
-	res.render("Roof Walkways");
+	res.render("Roof Walkways", {
+		title: "services"
+	});
 })
 app.get("/EdgeProtection", function(req,res){
-	res.render("Edge Protection");
+	res.render("Edge Protection", {
+		title: "services"
+	});
 })
 app.get("/AsbestosRemoval", function(req,res){
-	res.render("Asbestos Removal");
+	res.render("Asbestos Removal", {
+		title: "services"
+	});
 })
 
 app.get("/Gallery", function(req,res){
-	res.render("Gallery");
+	res.render("Gallery", {
+		title: "Gallery"
+	});
 })
 
 app.get("/Gallery/Southport", function(req,res){
-	res.render("Southport");
+	res.render("Southport", {
+		title: "Project"
+	});
 })
 
 app.get("/Gallery/Coombabah", function(req,res){
-	res.render("Coombabah");
+	res.render("Coombabah", {
+		title: "Project"
+	});
 })
 
 app.get("/Gallery/Alstonville", function(req,res){
-	res.render("Alstonville");
+	res.render("Alstonville", {
+		title: "Project"
+	});
 })
 app.get("/Gallery/Ascot", function(req,res){
-	res.render("Ascot");
+	res.render("Ascot", {
+		title: "Project"
+	});
 })
 
 // app.get("/:services", (req, res)=>{
@@ -99,7 +145,9 @@ app.get("/Gallery/Ascot", function(req,res){
 // })
 
 app.get("*", function(req,res){
-	res.render("unknown");
+	res.render("unknown", {
+		title: "Home"
+	});
 })
 
 const port = process.env.PORT || 3000;
